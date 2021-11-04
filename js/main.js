db8b94ffb9dd3b5d3f35e72e2cf3675ba00695dc
am1rcek <xusandadajonov4385@gmail.com>
2021-11-04 06:32:49 +0500

    wda

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let elSettingsBnt = document.querySelector(".header__btn--settings");
 let elAccounModal = document.querySelector(".account-modal");
+let elBody = document.querySelector("body");
 
 elSettingsBnt.addEventListener("click", function () {
     elAccounModal.classList.toggle("open-accoun-modal");
+    elBody.classList.toggle("body-transform");
 })
